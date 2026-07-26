c1358c61bd6df1b2e3c9ae0ad77cfc75abef0517
Tommy Seery <Tommy.Seery@infonetica.net>
2026-07-26 14:53:02 +0100

    refactor(apphost): simplify ASB subscription API [skip-e2e]

--- src/Concertable.B2B.AppHost.Extensions/B2BTopology.cs
+++ src/Concertable.B2B.AppHost.Extensions/B2BTopology.cs
@@ -5,10 +5,9 @@ using Concertable.Payment.Contracts.Events;
 public static class B2BTopology
 {
     public static AsbTopology AddB2BTopology(this AsbTopology topology) =>
-        topology.ForService(AppHostConstants.ServiceNames.B2B)
-            .Subscribe<CustomerReviewSubmittedEvent>()
-            .Subscribe<CredentialRegisteredEvent>()
-            .Subscribe<PaymentSucceededEvent>()
-            .Subscribe<PaymentFailedEvent>()
-            .Topology;
+        topology
+            .Subscribe<CustomerReviewSubmittedEvent>(AppHostConstants.ServiceNames.B2B)
+            .Subscribe<CredentialRegisteredEvent>(AppHostConstants.ServiceNames.B2B)
+            .Subscribe<PaymentSucceededEvent>(AppHostConstants.ServiceNames.B2B)
+            .Subscribe<PaymentFailedEvent>(AppHostConstants.ServiceNames.B2B);
 }
