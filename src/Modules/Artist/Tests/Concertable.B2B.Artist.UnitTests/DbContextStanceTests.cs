8a671bbef9d458759f5ec4f02ce5d66e36f31431
Tommy Seery <Tommy.Seery@infonetica.net>
2026-08-14 21:29:18 +0100

    Test B2B organisation identity lookups

--- src/Modules/Artist/Tests/Concertable.B2B.Artist.UnitTests/DbContextStanceTests.cs
+++ src/Modules/Artist/Tests/Concertable.B2B.Artist.UnitTests/DbContextStanceTests.cs
@@ -1,9 +1,14 @@
+using Concertable.B2B.Artist.Contracts;
 using Concertable.B2B.Artist.Domain.Entities;
 using Concertable.B2B.Artist.Infrastructure.Data;
+using Concertable.B2B.Artist.Infrastructure.Repositories;
 using Concertable.DataAccess.Application;
 using Concertable.Kernel.Identity;
+using Concertable.Kernel.ValueObjects;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
 using Moq;
+using NetTopologySuite.Geometries;
 
 namespace Concertable.B2B.Artist.UnitTests;
 
@@ -30,11 +35,58 @@ public sealed class DbContextStanceTests
         Assert.NotEmpty(tenantContext.Model.FindEntityType(typeof(ArtistEntity))!.GetDeclaredQueryFilters());
     }
 
+    [Fact]
+    public async Task OrgIdentityLookup_ReadsAcrossTenants()
+    {
+        var databaseName = Guid.NewGuid().ToString();
+        var databaseRoot = new InMemoryDatabaseRoot();
+        var provider = new ArtistConfigurationProvider();
+        var tenantId = Guid.NewGuid();
+
+        await using (var tenantContext = new TenantArtistDbContext(
+                         CreateInMemoryOptions<TenantArtistDbContext>(databaseName, databaseRoot),
+                         provider,
+                         Mock.Of<ITenantContext>(t => t.IsHost == true)))
+        {
+            var artist = ArtistEntity.Create(
+                Guid.NewGuid(),
+                "Artist name",
+                "About",
+                "banner",
+                "avatar",
+                new Point(0, 0),
+                new Address("County", "Town"),
+                "artist@example.com",
+                []);
+            artist.TenantId = tenantId;
+            tenantContext.Artists.Add(artist);
+            await tenantContext.SaveChangesAsync();
+        }
+
+        await using var readContext = new ArtistDbContext(
+            CreateInMemoryOptions<ArtistDbContext>(databaseName, databaseRoot),
+            provider);
+        var lookup = new ArtistOrgIdentityLookup(readContext);
+
+        Assert.Equal(
+            new ArtistOrgIdentity("Artist name", "County", "Town"),
+            await lookup.GetByTenantIdAsync(tenantId));
+        Assert.Null(await lookup.GetByTenantIdAsync(Guid.NewGuid()));
+    }
+
     private static DbContextOptions<TContext> CreateOptions<TContext>()
         where TContext : DbContext =>
         new DbContextOptionsBuilder<TContext>()
             .UseSqlServer(
                 "Server=localhost;Database=ContextStanceTests;User Id=sa;Password=Password123!;TrustServerCertificate=True",
                 sql => sql.UseNetTopologySuite())
+            .Options;
+
+    private static DbContextOptions<TContext> CreateInMemoryOptions<TContext>(
+        string databaseName,
+        InMemoryDatabaseRoot databaseRoot)
+        where TContext : DbContext =>
+        new DbContextOptionsBuilder<TContext>()
+            .UseInMemoryDatabase(databaseName, databaseRoot)
             .Options;
 }
