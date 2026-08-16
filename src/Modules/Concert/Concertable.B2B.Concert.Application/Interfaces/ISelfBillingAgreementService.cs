51ad8dfbc09bf570c8db20826207765a48fa72e0
Tommy Seery <Tommy.Seery@infonetica.net>
2026-08-16 12:51:10 +0100

    fix(b2b): compose expected result failures

--- src/Modules/Concert/Concertable.B2B.Concert.Application/Interfaces/ISelfBillingAgreementService.cs
+++ src/Modules/Concert/Concertable.B2B.Concert.Application/Interfaces/ISelfBillingAgreementService.cs
@@ -1,4 +1,5 @@
 using Concertable.B2B.Concert.Application.DTOs;
+using Concertable.B2B.Concert.Application.Errors;
 using Concertable.B2B.Concert.Application.Requests;
 
 namespace Concertable.B2B.Concert.Application.Interfaces;
@@ -7,7 +8,9 @@ internal interface ISelfBillingAgreementService
 {
     Task<SelfBillingAgreementStatusDto> GetStatusAsync(CancellationToken ct = default);
 
-    Task GrantAsync(ESignatureRequest eSignature, CancellationToken ct = default);
+    Task<UnitResult<GrantSelfBillingAgreementError>> GrantAsync(
+        ESignatureRequest eSignature,
+        CancellationToken ct = default);
 
     Task<FileDownload> GetPdfAsync(CancellationToken ct = default);
 }
