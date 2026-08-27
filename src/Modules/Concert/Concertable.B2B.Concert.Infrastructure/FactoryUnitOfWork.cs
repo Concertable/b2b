26876f26e9779dfbbb105c170a14149568eabd07
Tommy Seery <Tommy.Seery@infonetica.net>
2026-08-27 15:27:43 +0100

    refactor(concert): use shared unit of work boundary

--- src/Modules/Concert/Concertable.B2B.Concert.Infrastructure/FactoryUnitOfWork.cs
+++ src/Modules/Concert/Concertable.B2B.Concert.Infrastructure/FactoryUnitOfWork.cs
@@ -4,8 +4,8 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Concertable.B2B.Concert.Infrastructure;
 
-internal interface IUnitOfWorkBoundary : Concertable.B2B.DataAccess.Application.IUnitOfWorkBoundary<ConcertDbContext>;
+internal interface IUnitOfWorkBoundary : Concertable.DataAccess.Application.IUnitOfWorkBoundary<ConcertDbContext>;
 
 internal sealed class FactoryUnitOfWork(IDbContextFactory<ConcertDbContext> dbContextFactory)
-    : Concertable.B2B.DataAccess.Infrastructure.FactoryUnitOfWork<ConcertDbContext>(dbContextFactory),
+    : Concertable.DataAccess.Infrastructure.FactoryUnitOfWork<ConcertDbContext>(dbContextFactory),
         IUnitOfWorkBoundary;
