5bd188af6363c115b70b75b7557edb9dac6bb793
Tommy Seery <Tommy.Seery@infonetica.net>
2026-08-30 22:53:20 +0100

    fix(b2b): migrate pending application index

--- src/Modules/Application/Concertable.B2B.Application.Infrastructure/Data/Migrations/20260830213802_AlignApplicationModel.Designer.cs
+++ src/Modules/Application/Concertable.B2B.Application.Infrastructure/Data/Migrations/20260830213802_AlignApplicationModel.Designer.cs
@@ -5,6 +5,7 @@ using Concertable.B2B.Application.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -12,9 +13,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Concertable.B2B.Application.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260830213802_AlignApplicationModel")]
+    partial class AlignApplicationModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
