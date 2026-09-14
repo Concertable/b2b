Concertable/Concertable

refactor(b2b): one composed operation claim across the lifecycle aggregates
Five long-running operations anchored themselves to a row with an operation id
in three different shapes: the caller minted for acceptance, the entity minted
for settlement with a separate Ensure that threw two ways, and both
cancellations minted unconditionally. Nothing shared a vocabulary, so an
operation spanning entities could not reason about a claim uniformly.

OperationClaim is now composed into each aggregate, following the EventRaiser
precedent. Claim() mints on first use and resumes thereafter, Claim(id) takes an
id the caller already holds and validates before assigning, and IsHeldBy
verifies without claiming -- so "who mints" and "claim versus verify" stop being
shapes. EnsureSettlementOperation's two throws collapse into one.

Claim is internal, with InternalsVisibleTo for the three module Domain
assemblies, so only an owning aggregate can take a claim behind whatever
transition gates it. OperationId and IsHeldBy stay public because the mapped
path has to remain queryable.

It maps as an owned entity type, not a complex type: EF 10 cannot put an index
on a complex-type member, and every one of these five columns carries a unique
index. The index moves inside the OwnsOne builder, OwnsRequiredClaim keeps
Bookings.OperationId non-nullable under its unfiltered index, and
has-pending-model-changes reports no model change on all three of
ApplicationDbContext, BookingDbContext and ConcertDbContext -- same columns,
same index names, no migration.

IHasCancellationClaim lets one WithCancellationClaim query replace the identical
SingleOrDefaultAsync that Booking's and Concert's outcome processors each
hand-rolled. The settlement and acceptance claims get no interface: every caller
of those holds the concrete entity, so it would have no consumer.

Fixes a latent double refund. Both BeginCancellation methods minted a fresh
operation id on every call while BeginSettlement reused, and
Cancell

api/Concertable.B2B/src/Concertable.B2B.DataAccess/Concertable.B2B.DataAccess.Application/OperationClaim.cs
@@ -1,0 +1,35 @@
+namespace Concertable.B2B.DataAccess.Application;
+
+/// <summary>
+/// Anchors a long-running operation to the row it acts on, so a retried request resumes the same
+/// operation instead of starting a second one. Composed into an entity — one instance per claimable
+/// operation — and mapped as an owned entity type sharing the entity's table, so a claim travels
+/// inside the owner's concurrency token.
+/// </summary>
+/// <remarks>
+/// <see cref="Claim()"/> mints on first use and resumes thereafter; <see cref="Claim(Guid)"/> takes an
+/// id the caller already holds. Both reject a rival operation. <see cref="IsHeldBy"/> verifies without
+/// claiming. Claiming is internal so only an owning aggregate can take a claim, behind whatever
+/// transition gates it; everything else reads the mapped <see cref="OperationId"/>.
+/// </remarks>
+public sealed class OperationClaim
+{
+    public Guid? OperationId { get; private set; }
+
+    public bool IsHeldBy(Guid operationId) =>
+        operationId != Guid.Empty && OperationId == operationId;
+
+    internal Guid Claim() => OperationId ?? Claim(Guid.NewGuid());
+
+    internal Guid Claim(Guid operationId)
+    {
+        if (operationId == Guid.Empty)
+            throw new ArgumentException("An operation id is required.", nameof(operationId));
+        if (OperationId is { } held && held != operationId)
+            throw new InvalidOperationException(
+                $"The row is already claimed by operation {held}, not {operationId}.");
+
+        OperationId = operationId;
+        return operationId;
+    }
+}

Concertable/b2b

api/src/Concertable.B2B.DataAccess/Concertable.B2B.DataAccess.Application/OperationClaim.cs
@@ -1,0 +1,35 @@
+namespace Concertable.B2B.DataAccess.Application;
+
+/// <summary>
+/// Anchors a long-running operation to the row it acts on, so a retried request resumes the same
+/// operation instead of starting a second one. Composed into an entity — one instance per claimable
+/// operation — and mapped as an owned entity type sharing the entity's table, so a claim travels
+/// inside the owner's concurrency token.
+/// </summary>
+/// <remarks>
+/// <see cref="Claim()"/> mints on first use and resumes thereafter; <see cref="Claim(Guid)"/> takes an
+/// id the caller already holds. Both reject a rival operation. <see cref="IsHeldBy"/> verifies without
+/// claiming. Claiming is internal so only an owning aggregate can take a claim, behind whatever
+/// transition gates it; everything else reads the mapped <see cref="OperationId"/>.
+/// </remarks>
+public sealed class OperationClaim
+{
+    public Guid? OperationId { get; private set; }
+
+    public bool IsHeldBy(Guid operationId) =>
+        operationId != Guid.Empty && OperationId == operationId;
+
+    internal Guid Claim() => OperationId ?? Claim(Guid.CreateVersion7());
+
+    internal Guid Claim(Guid operationId)
+    {
+        if (operationId == Guid.Empty)
+            throw new ArgumentException("An operation id is required.", nameof(operationId));
+        if (OperationId is { } held && held != operationId)
+            throw new InvalidOperationException(
+                $"The row is already claimed by operation {held}, not {operationId}.");
+
+        OperationId = operationId;
+        return operationId;
+    }
+}